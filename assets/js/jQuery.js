4f589ea5f870eb1e36dcf67a52741b56201679a0
alejandro-sierra <alejandrosierraballesta@gmail.com>
2022-02-15 12:47:28 +0100

    no funciona bien el css

---
+++ assets/js/jQuery.js
@@ -0,0 +1,16 @@
+/*
+$(document).ready(function(){
+    $(".fa-arrow-up").css("display", "none");
+       $(window).scroll(function(){
+           if($(window).scrollTop() > 200){
+               $(".fa-arrow-up").fadeIn("slow");
+           }
+           else {
+               $(".fa-arrow-up").fadeOut("slow");
+           }
+       });
+});
+ */
+   $(document).ready(function(){
+    $(".flechaArriba").css("display", "none");
+   });
